ecf5ea18db693d1e792aad2de23fc13381bbfb78
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-12-20 23:06:11 +0600

    Create Inline functions.cpp

---
+++ C++/Inline Functions/Inline functions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+inline int product(int a, int b) {
+    return a*b;
+}
+
+int main() {
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+    cout << product(2, 3) << endl;
+   
+    return 0;
+}
